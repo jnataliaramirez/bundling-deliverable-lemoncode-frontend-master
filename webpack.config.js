b22586e47f317dd6fa8fa076ff77014603e21070
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-06-08 21:05:34 +0200

    Do config initial, install webpack, babel and begin first files

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: {
+    app: ["./src/index.js"],
+  },
+  module:  {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
